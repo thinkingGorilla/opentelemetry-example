5f54146cfff4f960bb1e9c50aac87e6453817f9a
thinkingGorilla <4zxzxz4@gmail.com>
2025-07-01 16:06:01 +0900

    추적 기능 추가
    - `configure_trace` 함수가 트레이서를 반환하도록 수정
    - `browse` 함수 추가 및 매장 방문 메시지 출력
    - 메인 실행부에서 스팬 생성 및 종료 로직 추가

--- shopper.py
+++ shopper.py
@@ -9,6 +9,13 @@ def configure_trace():
     provider = TracerProvider()
     provider.add_span_processor(span_processor)
     trace.set_tracer_provider(provider)
+    return trace.get_tracer("shopper.py", "0.0.1")
+
+def browse():
+    print("visiting the grocery store")
 
 if __name__ == "__main__":
-    configure_trace()
+    tracer = configure_trace()
+    span = tracer.start_span("visit store")
+    browse()
+    span.end()
