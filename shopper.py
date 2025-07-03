3a5ee1cfad7d919b54b0aef6ec9e97cd3926000f
thinkingGorilla <4zxzxz4@gmail.com>
2025-07-03 16:46:34 +0900

    HTTP 요청 상태 설정 로직 및 URL 수정
    - `browse` 함수에서 잘못된 URL로 수정하여 에러 상황 테스트 가능
    - HTTP 요청 상태에 따라 스팬 상태를 `StatusCode.OK` 또는 `StatusCode.ERROR`로 설정
    - `set_status_on_exception` 옵션 추가로 예외 발생 시 상태 자동 설정
    - 불필요한 예외 처리 로직 제거 및 코드 간소화

--- shopper.py
+++ shopper.py
@@ -3,6 +3,7 @@ import requests
 from opentelemetry import trace
 from opentelemetry.propagate import inject
 from opentelemetry.semconv.trace import HttpFlavorValues, SpanAttributes
+from opentelemetry.trace import StatusCode, Status
 
 from common import configure_tracer
 
@@ -13,9 +14,9 @@ tracer = configure_tracer("shopper", "0.1.2")
 def browse():
     print("visiting the grocery store")
     with tracer.start_as_current_span(
-            "web request", kind=trace.SpanKind.CLIENT, record_exception=False
+            "web request", kind=trace.SpanKind.CLIENT, record_exception=False, set_status_on_exception=True
     ) as span:
-        url = "http://localhost:5000/products"
+        url = "http://localhost:5000/products/invalid"
         span.set_attributes(
             {
                 SpanAttributes.HTTP_REQUEST_METHOD: "GET",
@@ -28,13 +29,17 @@ def browse():
         inject(headers)
         span.add_event("about to send a request")
         resp = requests.get(url, headers=headers)
+        if resp:
+            span.set_status(Status(StatusCode.OK))
+        else:
+            span.set_status(Status(StatusCode.ERROR, "status code: {}".format(resp.status_code)))
         span.add_event("request sent", attributes={"rul": url}, timestamp=0)
-        try:
-            url = "invalid_url"
-            resp = requests.get(url, headers=headers)
-            span.add_event("request sent", attributes={"rul": url}, timestamp=0)
-        except Exception as err:
-            span.record_exception(err)
+        # try:
+        #     url = "invalid_url"
+        #     resp = requests.get(url, headers=headers)
+        #     span.add_event("request sent", attributes={"rul": url}, timestamp=0)
+        # except Exception as err:
+        #     span.record_exception(err)
         span.set_attribute(SpanAttributes.HTTP_STATUS_CODE, resp.status_code)
         add_item_to_cart("orange", 5)
 
