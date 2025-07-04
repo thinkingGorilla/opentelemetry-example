e96bd8f7937c36ef06a594a28c79b3a9fe1824d5
thinkingGorilla <4zxzxz4@gmail.com>
2025-07-04 17:58:14 +0900

    콘솔 메트릭 내보내기 설정 및 카운터 추가

--- chapter5/metrics.py
+++ chapter5/metrics.py
@@ -1,18 +1,24 @@
-from opentelemetry.exporter.prometheus import PrometheusMetricReader
 from opentelemetry.metrics import get_meter_provider, set_meter_provider
 from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import (
+    ConsoleMetricExporter,
+    PeriodicExportingMetricReader
+)
 from opentelemetry.sdk.resources import Resource
-from prometheus_client import start_http_server
+
+
+# from opentelemetry.exporter.prometheus import PrometheusMetricReader
+# from prometheus_client import start_http_server
 
 
 def configure_meter_provider():
-    start_http_server(port=8080, addr="localhost")
-    reader = PrometheusMetricReader()
-    provider = MeterProvider(metric_readers=[reader], resource=Resource.create())
+    # start_http_server(port=8080, addr="localhost")
+    # reader = PrometheusMetricReader()
+    # provider = MeterProvider(metric_readers=[reader], resource=Resource.create())
 
-    # exporter = ConsoleMetricExporter()
-    # reader = PeriodicExportingMetricReader(exporter, export_interval_millis=5000)
-    # provider = MeterProvider(resource=Resource.create())
+    exporter = ConsoleMetricExporter()
+    reader = PeriodicExportingMetricReader(exporter, export_interval_millis=5000)
+    provider = MeterProvider(metric_readers=[reader], resource=Resource.create())
 
     set_meter_provider(provider)
 
@@ -25,3 +31,10 @@ if __name__ == '__main__':
         schema_url="https://opentelemetry.io/schemas/1.9.0",
     )
     input("Press any key to exit...")
+    counter = meter.create_counter(
+        "item_sold",
+        unit="items",
+        description="Total item sold"
+    )
+    counter.add(6, {"local": "fr-FR", "country": "CA"})
+    counter.add(1, {"local": "es-ES"})
