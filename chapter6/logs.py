51524e0b16a3f72a07df4b113e48a6687efe9fd7
thinkingGorilla <4zxzxz4@gmail.com>
2025-07-09 15:23:20 +0900

    표준 로그 라이브러리 사용
    - 복잡한 설정 없이 표준 로그 라이브러리를 사용하여 로그 시그널 생성 구현

--- chapter6/logs.py
+++ chapter6/logs.py
@@ -1,11 +1,9 @@
-import time
+import logging
 
 from opentelemetry._logs import (
-    set_logger_provider,
-    get_logger_provider
+    set_logger_provider
 )
-from opentelemetry._logs.severity import SeverityNumber
-from opentelemetry.sdk._logs import LoggerProvider, LogRecord
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
 from opentelemetry.sdk._logs.export import ConsoleLogExporter, BatchLogRecordProcessor
 from opentelemetry.sdk.resources import Resource
 
@@ -18,14 +16,7 @@ def configure_logger_provider():
 
 if __name__ == '__main__':
     configure_logger_provider()
-    logger = get_logger_provider().get_logger(
-        "shopper",
-        "0.1.2",
-    )
-    logger.emit(
-        LogRecord(
-            timestamp=time.time_ns(),
-            body="first log line",
-            severity_number=SeverityNumber.INFO,
-        )
-    )
+    logger = logging.getLogger(__file__)
+    handler = LoggingHandler()
+    logger.addHandler(handler)
+    logger.warning("second log line", extra={"key1": "val1"})
