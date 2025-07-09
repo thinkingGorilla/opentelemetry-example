b8170b820976859324f6a1d4fa1567d6947dfbd6
thinkingGorilla <4zxzxz4@gmail.com>
2025-07-09 13:58:05 +0900

    챕터 6 로깅 시그널
    - SeverityNumber 적용

--- chapter6/logs.py
+++ chapter6/logs.py
@@ -1,5 +1,11 @@
-from opentelemetry._logs import set_logger_provider
-from opentelemetry.sdk._logs import LoggerProvider
+import time
+
+from opentelemetry._logs import (
+    set_logger_provider,
+    get_logger_provider
+)
+from opentelemetry._logs.severity import SeverityNumber
+from opentelemetry.sdk._logs import LoggerProvider, LogRecord
 from opentelemetry.sdk._logs.export import ConsoleLogExporter, BatchLogRecordProcessor
 from opentelemetry.sdk.resources import Resource
 
@@ -9,3 +15,17 @@ def configure_logger_provider():
     set_logger_provider(provider)
     exporter = ConsoleLogExporter()
     provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
+
+if __name__ == '__main__':
+    configure_logger_provider()
+    logger = get_logger_provider().get_logger(
+        "shopper",
+        "0.1.2",
+    )
+    logger.emit(
+        LogRecord(
+            timestamp=time.time_ns(),
+            body="first log line",
+            severity_number=SeverityNumber.INFO,
+        )
+    )
